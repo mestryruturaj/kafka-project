59cd197f177ec2b1a5a19ec63416d352224b6528
Ruturaj Mestry <mestryruturaj@gmail.com>
2025-06-19 08:05:04 +0530

    Dev: EbKafkaConsumer with main method, and shutdown consumer gracefully logic.

--- kafka-basics/src/main/java/com/enscryptingbytes/practice/consumer/EbKafkaConsumer.java
+++ kafka-basics/src/main/java/com/enscryptingbytes/practice/consumer/EbKafkaConsumer.java
@@ -3,6 +3,7 @@ package com.enscryptingbytes.practice.consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +17,7 @@ public class EbKafkaConsumer {
 
     public static void main(String[] args) {
         EbKafkaConsumer ebKafkaConsumer = new EbKafkaConsumer();
-        ebKafkaConsumer.consume();
+        ebKafkaConsumer.consumeWithGracefulShutdown();
     }
 
     public void consume() {
@@ -42,6 +43,59 @@ public class EbKafkaConsumer {
             for (ConsumerRecord<String, String> record : records) {
                 logger.info(String.format("topic=%s\t partition=%s\t key=%s\t value=%s%n", record.topic(), record.partition(), record.key(), record.value()));
             }
+        }
+    }
+
+    public void consumeWithGracefulShutdown() {
+        logger.info("Consume with graceful shutdown.");
+
+        String groupId = "my-java-application";
+        String topic = "kafka_project";
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+
+        final Thread executorThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                logger.info("Initiated shutdown hook.");
+                kafkaConsumer.wakeup();
+
+                try {
+                    executorThread.join();
+                } catch (InterruptedException e) {
+                    logger.info("Polling has stopped.");
+                }
+            }
+        });
+
+
+        try {
+            kafkaConsumer.subscribe(List.of(topic));
+
+            while (true) {
+                logger.info("Consumer is Polling!!!");
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(1000));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    logger.info(String.format("topic=%s\t partition=%s\t key=%s\t value=%s%n", record.topic(), record.partition(), record.key(), record.value()));
+                }
+            }
+        } catch(WakeupException e) {
+            logger.info("Consumer shutdown initiated.");
+        } catch (Exception e) {
+            logger.error("Unexpected error occurred, ", e);
+        } finally {
+            kafkaConsumer.close();
+            logger.info("Consumer shutdown completed.");
         }
     }
 }
